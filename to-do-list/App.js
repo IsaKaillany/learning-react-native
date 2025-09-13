db969bf1573ce4b158c4ce9a1ae7a6363ca56cc2
Isa Kaillany <isakaillany123@gmail.com>
2025-09-13 15:35:45 -0300

    creating a to do list

--- to-do-list/App.js
+++ to-do-list/App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    FlatList,
+} from "react-native";
+
+import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
+import Tarefa from "./src/Tarefa";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [tarefa, setTarefa] = useState("");
+    const [list, setList] = useState([
+        {
+            key: 1,
+            item: "comprar pao",
+        },
+        {
+            key: 2,
+            item: "estudar rn",
+        },
+    ]);
+
+    function handleAdd() {
+        alert(tarefa);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Tarefas</Text>
+
+            <View style={styles.containerInput}>
+                <TextInput
+                    placeholder="Digite sua tarefa..."
+                    style={styles.input}
+                    value={tarefa}
+                    onChangeText={(text) => setTarefa(text)}
+                />
+
+                <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
+                    <FontAwesome name="plus" size={20} color="#fff" />
+                </TouchableOpacity>
+            </View>
+
+            <FlatList
+                data={list}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) => <Tarefa data={item} />}
+                style={styles.list}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#22272e",
+        paddingTop: 28,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 24,
+        color: "#fff",
+        marginTop: "5%",
+        paddingStart: "5%",
+        marginBottom: 12,
+    },
+    containerInput: {
+        flexDirection: "row",
+        width: "100%",
+        height: 44,
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 22,
+    },
+    input: {
+        width: "75%",
+        backgroundColor: "#fbfbfb",
+        height: 44,
+        borderRadius: 4,
+        paddingHorizontal: 8,
+    },
+    buttonAdd: {
+        width: "15%",
+        height: 44,
+        backgroundColor: "#73f7ff",
+        marginLeft: 8,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 4,
+    },
+    list: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingStart: '4%',
+        paddingEnd: '4%',
+    }
 });
---
+++ to-do-list/src/Tarefa.js
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function Tarefa({ data }) {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button}>
+                <FontAwesome name="trash" size={20} color="#22272e" />
+            </TouchableOpacity>
+
+            <Text>{data.item}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "rgba(196, 196, 196, 0.20)",
+        marginTop: 12,
+        padding: 12,
+        borderRadius: 4,
+        flexDirection: "row",
+
+    },
+    button: {
+        marginRight: 8
+    }
+});
